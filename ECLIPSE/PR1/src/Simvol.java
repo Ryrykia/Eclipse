82e8c38a19d4f4ba1359e585d88738a70a13119a
Kochneva Kristina <103591010+Ryrykia@users.noreply.github.com>
2022-11-07 20:56:13 +0500

    Add files via upload

---
+++ ECLIPSE/PR1/src/Simvol.java
@@ -0,0 +1,11 @@
+
+открытый класс Печать {
+
+	public static void main(String[] args) {
+
+		//Задание 1
+		System.out.println("а=1, c=a+1 m=2, n=3 решение принадлежит \nb=6, d=b*2-1 p=4, g=5 нахождение корней ");
+
+	}
+
+}
